e3bb11d12ddd65a7e6e7b310df9b905544c78a50
Elliot Blower <zkac154@cim-ts-node-01.cc.rhul.local>
2023-01-29 16:03:34 +0000

    Change Staff table to include hash+salt fields
    The SQL for creating the database was modified to use a hash and salt
    rather than a plaintext password. The Database.java was also cleaned up
    a little.

--- src/java/Database.java
+++ src/java/Database.java
@@ -5,20 +5,22 @@ import java.sql.*;
 import java.util.*;
 
 public class Database {
+	static Connection connection;
 	
-	// NOTE: You will need to change some variables from START to END.
 	public static void main(String[] argv) throws SQLException {
-
-		Connection connection = connectToDatabase();
+		connection = connectToDatabase();
 		if (connection != null) {
-			System.out.println("SUCCESS: You made it!"
-					+ "\n\t You can now take control of your database!\n");
+			System.out.println(
+					"SUCCESS: Connected to database successfully.");
 		} else {
 			System.out.println("ERROR: \tFailed to make connection!");
 			System.exit(1);
 		}
-		// Now we're ready to use the DB. You may add your code below this line.
-		
+
+		initialiseTables();
+	}
+
+	public static void initialiseTables() throws SQLException {
 		Statement statement = connection.createStatement();
 
 		statement.execute("DROP TABLE IF EXISTS Menu;");
@@ -39,67 +41,66 @@ public class Database {
 				CREATE TABLE Staff (
 					StaffID INTEGER NOT NULL,
 					username VARCHAR(255) NOT NULL,
-					password VARCHAR(255) NOT NULL,
+					password_hash VARCHAR(255) NOT NULL,
+					salt VARCHAR(255) NOT NULL,
 					PRIMARY KEY (StaffID)
 				);
 				""";
 		statement.executeUpdate(MenuTable);
 		statement.executeUpdate(StaffTable);
 	}
-	
-	
-	public static void initialiseTable(String tableName, Statement statement) throws SQLException {
-	    try{
-	        FileInputStream fis = new FileInputStream(tableName);
-	        Scanner sc = new Scanner(fis);
-	        String[] arrOfStr = sc.nextLine().split(",");
 
-	        while (sc.hasNextLine()){
-	        	
-	            statement.executeUpdate(Insert(arrOfStr, tableName));
-	            arrOfStr = sc.nextLine().split(",");
-	            
-	        }
-	        statement.executeUpdate(Insert(arrOfStr, tableName));
-	        
-	        sc.close();
-	    }
-	    catch(IOException e) {
-	      e.printStackTrace();
-	    }
+	public static void initialiseTableFromFile(String tableName, Statement statement)
+			throws SQLException {
+		try {
+			FileInputStream fis = new FileInputStream(tableName);
+			Scanner sc = new Scanner(fis);
+			String[] arrOfStr = sc.nextLine().split(",");
+
+			while (sc.hasNextLine()) {
+
+				statement.executeUpdate(Insert(arrOfStr, tableName));
+				arrOfStr = sc.nextLine().split(",");
+
+			}
+			statement.executeUpdate(Insert(arrOfStr, tableName));
+
+			sc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
+
+
 	public static String Insert(String[] arr, String tableName) {
 		String temp = "";
 		String sql = "";
-		for (String a: arr) {
-			if (IsInt(a) ==true) {
-				temp += a+",";
-			}
-			else {
-				temp += "'"+a+"',";
+		for (String a : arr) {
+			if (IsInt(a) == true) {
+				temp += a + ",";
+			} else {
+				temp += "'" + a + "',";
 			}
 		}
-		temp = temp.substring(0, temp.length()-1);
-		sql = "INSERT INTO "+tableName+ " VALUES("+temp+");";
+		temp = temp.substring(0, temp.length() - 1);
+		sql = "INSERT INTO " + tableName + " VALUES(" + temp + ");";
 		return sql;
 	}
-	
-	public static boolean IsInt(String str){
+
+	public static boolean IsInt(String str) {
 		try {
 			Integer.parseInt(str);
-		    return true;
-		} catch(NumberFormatException e){
+			return true;
+		} catch (NumberFormatException e) {
 			return false;
 		}
 	}
-	
-	
+
+
 	public static Connection connectToDatabase() {
 		System.out.println("------ Testing PostgreSQL JDBC Connection ------");
-		//String user = "postgres"; for offline postres
-		//String password = "Vietnam1";  for offline postres
+		// String user = "postgres"; for offline postgres
+		// String password = "Vietnam1"; for offline postgres
 
 		String user = "group35";
 		String password = "eibahv";
@@ -107,7 +108,7 @@ public class Database {
 		Connection connection = null;
 		try {
 			String protocol = "jdbc:postgresql://";
-			//String dbName = "/postgres";    offline postres
+			// String dbName = "/postgres"; offline postgres
 			String dbName = "/CS2810/group35";
 			String fullURL = protocol + database + dbName;
 			connection = DriverManager.getConnection(fullURL, user, password);
@@ -115,9 +116,8 @@ public class Database {
 		} catch (SQLException e) {
 			String errorMsg = e.getMessage();
 			if (errorMsg.contains("authentication failed")) {
-				System.out.println("ERROR: \tDatabase password is incorrect. Have you changed the password string above?");
-				System.out.println("\n\tMake sure you are NOT using your university password.\n"
-						+ "\tYou need to use the password that was emailed to you!");
+				System.out.println(
+						"ERROR: \tDatabase password is incorrect.");
 			} else {
 				System.out.println("Connection failed! Check output console.");
 				e.printStackTrace();
