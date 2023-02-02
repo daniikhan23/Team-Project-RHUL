76b5716534205a2f0003dfe140db4fb14c5b8c0e
Elliot Blower <zkac154@cim-ts-node-01.cc.rhul.local>
2023-02-02 17:06:05 +0000

    Get password hash and salt from database in Auth.java

--- src/main/java/Auth.java
+++ src/main/java/Auth.java
@@ -1,10 +1,25 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Auth {
-	public boolean validateLogin(String username, String password, Connection dbConnection) {
+	public boolean validateLogin(String username, String password, Connection dbConnection) throws SQLException {
 		/* check if username in db, if so make sure sha512(password + salt) = password_hash in db */
+		Connection db_conn = Database.getConnection();
+		PreparedStatement statement = db_conn.prepareStatement("SELECT password_hash, salt FROM Staff WHERE username = ?");
+		statement.setString(1, username);
+		ResultSet result = statement.executeQuery();
+		
+		if (!result.next()) /* not found in db */
+			return false;
+		
+		byte[] pass_hash = result.getBytes(1);
+		byte[] salt = result.getBytes(2);
+		
 		return false;
 	}
 }
