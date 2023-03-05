1a19a2100bbecdfc141a1ec43f1cff0578591f06
DannyAbdi <dannyabdi13@gmail.com>
2023-03-05 14:31:19 +0000

    Added getOrder method to KitchenOrder

--- src/main/java/Order/Kitchen/KitchenOrder.java
+++ src/main/java/Order/Kitchen/KitchenOrder.java
@@ -1,6 +1,5 @@
 package Order.Kitchen;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +9,15 @@ import DB.connection.Database;
 
 public class KitchenOrder {
 	
-	public ResultSet getOrderTimes() {
+	public ResultSet getOrder(int tableNo) throws ClassNotFoundException, SQLException {
+		 String sql = "SELECT * FROM OrderTable WHERE TableNo = " + tableNo + " AND CompletePhase = 0;";
+		 Connection connection = Database.connectToDatabase();
+		 Statement statement = connection.createStatement();
+		 ResultSet rs = statement.executeQuery(sql);
+		 return rs;
+	 }
+	
+	public ResultSet getOrderTimes() throws ClassNotFoundException, SQLException {
 		 String sql = "SELECT OrderID, timeStarted FROM OrderTable WHERE CompletePhase = 0;";
 		 Connection connection = Database.connectToDatabase();
 		 Statement statement = connection.createStatement();
