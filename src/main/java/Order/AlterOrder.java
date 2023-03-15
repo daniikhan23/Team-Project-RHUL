a9c102aab7112a5dff5b1d2cc4b71661d92cd4c5
Leo Nguyen <zkac263@live.rhul.ac.uk>
2023-03-15 17:42:07 +0000

    added interaction for the waiter and kitchen@

--- src/main/java/Order/AlterOrder.java
+++ src/main/java/Order/AlterOrder.java
@@ -39,5 +39,19 @@ public class AlterOrder { //add all order to accepted order
 		statement.execute(SQL);
 	}
 	
+	public void CompleteOrder(int order) throws SQLException, ClassNotFoundException {
+		Connection connection = Database.connectToDatabase();
+		Statement statement = connection.createStatement();
+		
+		String SQL = "UPDATE ordertable SET CompletePhase = 4 WHERE OrderNO = "+order+";";
+		statement.execute(SQL);
+	}
 	
+	public void CompleteItem(int order, String item) throws SQLException, ClassNotFoundException {
+		Connection connection = Database.connectToDatabase();
+		Statement statement = connection.createStatement();
+		
+		String SQL = "UPDATE ordertable SET CompletePhase = 3 WHERE OrderNO = "+order+" AND orderItem = '"+item+"';";
+		statement.execute(SQL);
+	}
 }
---
+++ src/main/java/Order/Kitchen/cancelOrder.java
@@ -0,0 +1,47 @@
+package Order.Kitchen;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import Order.AlterOrder;
+
+public class cancelOrder extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+		
+			
+			if (request.getParameter("OrderItem") != null) {
+				String table = request.getParameter("OrderCancel");
+				String item = request.getParameter("OrderItem");
+				int Table = Integer.parseInt(table);
+				AlterOrder order = new AlterOrder();
+				try {
+					order.cancelitem(Table, item);
+				} catch (ClassNotFoundException | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else {
+				String table = request.getParameter("OrderCancel");
+				AlterOrder order = new AlterOrder();
+				int Table = Integer.parseInt(table);
+				try {
+					order.cancelorder(Table);
+				} catch (ClassNotFoundException | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		
+		response.sendRedirect("kitchen.jsp");
+	}
+	
+}
